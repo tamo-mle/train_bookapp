601e544640d81e2b18bf76a40e7e40cf2336c41b
tamo-mle <tak.apple23@yahoo.ne.jp>
2021-08-09 04:59:28 +0000

    モデル修正

--- bookproject/book/models.py
+++ bookproject/book/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Book (models.Model):
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
