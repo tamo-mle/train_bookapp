2e0ff8f27ed3976a8bc5dd6011f88ff6a7e95abe
tamo-mle <tak.apple23@yahoo.ne.jp>
2021-08-09 05:19:32 +0000

    ListView作成

---
+++ bookproject/book/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import BookList
+
+urlpatterns = [
+    path('list', BookList.as_view()),
+]
--- bookproject/book/views.py
+++ bookproject/book/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Book
 
 # Create your views here.
+class BookList(ListView):
+    template_name = 'list.html'
+    model = Book
